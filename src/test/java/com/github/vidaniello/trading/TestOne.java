709c9751d5dde6533bc260fec64ec9695b0931a5
vidaniello@gmail.com <vidaniello@gmail.com@DESKTOP-4MA1LKV>
2020-10-17 20:09:49 +0200

    First commit

---
+++ src/test/java/com/github/vidaniello/trading/TestOne.java
@@ -0,0 +1,18 @@
+package com.github.vidaniello.trading;
+
+import org.junit.Test;
+
+public class TestOne {
+	
+	@Test
+	public void testOne() {
+		
+		try {
+			
+		} catch (Exception e) {
+			throw new AssertionError(e);
+		}
+		
+	}
+
+}
